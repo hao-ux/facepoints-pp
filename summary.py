fc45da6a7414697414e695215ceb634bf212c639
hao-ux <73505163+hao-ux@users.noreply.github.com>
2022-05-21 19:40:43 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -1,7 +1,7 @@
 import paddle
-from net.backbone import FaceKeyPointsNet
+from net.backbone import FaceKeyPointsNetBody
 # 输出网络结构
 
 if __name__ == '__main__':
-    model = paddle.Model(FaceKeyPointsNet(68, backbone='mobilenetv2'))
+    model = paddle.Model(FaceKeyPointsNetBody(68, backbone='mobilenetv2'))
     model.summary((-1, 3, 224, 244))
