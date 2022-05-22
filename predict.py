7b64b2cbed104fd925a80f2ba6e6744d94990b38
hao-ux <73505163+hao-ux@users.noreply.github.com>
2022-05-22 12:00:23 +0800

    Update predict.py
    增加fps

--- predict.py
+++ predict.py
@@ -5,19 +5,29 @@ import matplotlib.image as mpimg
 
 facekeypointnet = FaceKeyPointsNet()
 
+# ----------------------- #
+# mode=1 测试单张图片预测时间
+# mode=0 测试单张图片效果展示
+mode = 1
+n=100 # 测试100次，仅在mode=1有效
 
 
 if __name__ == '__main__':
+    if mode == 0:
 
-    while True:
-        img = input('Input image filename:')
-        try:
-            image = mpimg.imread(img)
-        except:
-            print('Open Error! Try again!')
-            continue
-        else:
-            facekeypointnet.detect_image(image)
+        while True:
+            img = input('Input image filename:')
+            try:
+                image = mpimg.imread(img)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                facekeypointnet.detect_image(image)
 
+    elif mode == 1:
+        image = mpimg.imread('./img/face.png')
+        t = facekeypointnet.fps(image, n=100)
+        print("FPS:{}, n: {}, time: {}".format(1/t, n, t))
 
     
